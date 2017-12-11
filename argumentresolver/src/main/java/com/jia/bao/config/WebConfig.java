dce41e9432bec556534b406d0836121ff5bb42ed
majiabao <majiabaoIT@163.com>
2017-12-11 16:17:25 +0800

    C

--- argumentresolver/src/main/java/com/jia/bao/config/WebConfig.java
+++ argumentresolver/src/main/java/com/jia/bao/config/WebConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import javax.annotation.Resource;
-import java.lang.reflect.Method;
 import java.util.List;
 
 /**
@@ -19,21 +17,10 @@ public class WebConfig extends DelegatingWebMvcConfiguration {
     @Resource
     private ConfigurableBeanFactory configurableBeanFactory;
 
-    @Bean
     @Override
-    public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
-        RequestMappingHandlerAdapter adapter = super.requestMappingHandlerAdapter();
-        List<HandlerMethodArgumentResolver> argumentResolverList;
-        try {
-            Method method = RequestMappingHandlerAdapter.class.getDeclaredMethod("getDefaultArgumentResolvers");
-            method.setAccessible(true);
-            argumentResolverList = (List<HandlerMethodArgumentResolver>) method.invoke(adapter);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        argumentResolverList.add(0, requestParamNotEmptyResolver());
-        adapter.setArgumentResolvers(argumentResolverList);
-        return adapter;
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        super.addArgumentResolvers(argumentResolvers);
+        argumentResolvers.add(requestParamNotEmptyResolver());
     }
 
     @Bean
